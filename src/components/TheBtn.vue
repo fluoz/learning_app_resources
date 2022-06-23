cb02a1dea84b306d0a712060e44e73355f1a51eb
fluoz <hmunshi12@gmail.com>
2022-06-23 16:34:01 +0700

    add button component

---
+++ src/components/TheBtn.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    @click="clicked('stored')"
+    :class="isSelected === 'stored' ? activeCss : deactiveCss"
+  >
+    Stored Resources
+  </button>
+  <button
+    @click="clicked('add')"
+    :class="isSelected === 'add' ? activeCss : deactiveCss"
+  >
+    Add Resources
+  </button>
+</template>
+<script setup>
+import { ref, computed } from "vue";
+
+const isSelected = ref("stored");
+const activeCss = [
+  "bg-purple-900",
+  "w-48",
+  "text-white",
+  "h-12",
+  "font-semibold",
+];
+const deactiveCss = ["text-purple-900", "w-48", "h-12", "font-semibold"];
+
+const clicked = (btn) => {
+  isSelected.value = btn;
+};
+</script>
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-full h-24 bg-red-900 flex items-center justify-center">
+    <h1 class="text-white font-bold text-4xl">RememberMe</h1>
+  </div>
+</template>
+<style scoped></style>
